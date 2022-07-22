ac0b809c0bdcd2060892a7a44e0426f99e247c27
pandabear15 <nelsonysun@gmail.com>
2022-07-22 10:48:37 -0700

    Reverted the change and readded the inner class, as it turns out things break without it.

--- src/main/java/com/uid2/admin/audit/QLDBAuditMiddleware.java
+++ src/main/java/com/uid2/admin/audit/QLDBAuditMiddleware.java
@@ -8,7 +8,6 @@ import java.time.Instant;
 import java.util.List;
 
 public class QLDBAuditMiddleware implements AuditMiddleware{
-    private AuditHandler<RoutingContext> handler;
     private final AuditWriter auditWriter;
 
     protected QLDBAuditMiddleware(AuditWriter writer){
@@ -17,30 +16,39 @@ public class QLDBAuditMiddleware implements AuditMiddleware{
 
     @Override
     public Handler<RoutingContext> handle(AuditHandler<RoutingContext> handler) {
-        this.handler = handler;
-        return this::writeLog;
+        InnerAuditHandler auditHandler = new InnerAuditHandler(handler, auditWriter);
+        return auditHandler::writeLog;
     }
 
-    public void writeLog(RoutingContext rc){
-        List<OperationModel> modelList = handler.handle(rc);
-        if(modelList != null && rc.statusCode() < 300){
-            for(OperationModel model : modelList) {
-                AuditModel auditModel = new QLDBAuditModel(model.itemType, model.itemKey, model.actionTaken,
-                        getIPAddress(rc),
-                        ((IAuthorizable) rc.data().get("api-client")).getContact(),
-                        System.getenv("HOSTNAME"), Instant.now().getEpochSecond(), model.itemHash, model.summary);
-                auditWriter.writeLog(auditModel);
+    private static class InnerAuditHandler{
+        private final AuditHandler<RoutingContext> innerHandler;
+        private final AuditWriter auditWriter;
+        private InnerAuditHandler(AuditHandler<RoutingContext> handler, AuditWriter auditWriter) {
+            this.innerHandler = handler;
+            this.auditWriter = auditWriter;
+        }
+
+        public void writeLog(RoutingContext rc){
+            List<OperationModel> modelList = innerHandler.handle(rc);
+            if(modelList != null && rc.statusCode() < 300){
+                for(OperationModel model : modelList) {
+                    AuditModel auditModel = new QLDBAuditModel(model.itemType, model.itemKey, model.actionTaken,
+                            getIPAddress(rc),
+                            ((IAuthorizable) rc.data().get("api-client")).getContact(),
+                            System.getenv("HOSTNAME"), Instant.now().getEpochSecond(), model.itemHash, model.summary);
+                    auditWriter.writeLog(auditModel);
+                }
             }
         }
-    }
 
-    private static String getIPAddress(RoutingContext rc){
-        List<String> listIP = rc.request().headers().getAll("X-Forwarded-For");
-        if(listIP == null || listIP.isEmpty()){
-            return rc.request().remoteAddress().toString();
-        }
-        else{
-            return listIP.get(0); //arbitrary if multiple
+        private static String getIPAddress(RoutingContext rc){
+            List<String> listIP = rc.request().headers().getAll("X-Forwarded-For");
+            if(listIP == null || listIP.isEmpty()){
+                return rc.request().remoteAddress().toString();
+            }
+            else{
+                return listIP.get(0); //arbitrary if multiple
+            }
         }
     }
 }
