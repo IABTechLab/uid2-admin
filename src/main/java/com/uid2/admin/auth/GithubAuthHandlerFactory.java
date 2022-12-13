5ec2c42e67a4c8892976a4e942285a95a5c1cb1c
Aleksandrs Ulme <aleksandrs.ulme@thetradedesk.com>
2022-12-13 17:38:59 +0800

    Disabling GitHub Oauth2 for local development, added readme

--- src/main/java/com/uid2/admin/auth/GithubAuthHandlerFactory.java
+++ src/main/java/com/uid2/admin/auth/GithubAuthHandlerFactory.java
@@ -8,8 +8,10 @@ import io.vertx.ext.web.Route;
 import io.vertx.ext.web.handler.AuthHandler;
 import io.vertx.ext.web.handler.OAuth2AuthHandler;
 
+import static com.uid2.admin.auth.AuthUtils.isAuthDisabled;
+
 public class GithubAuthHandlerFactory implements IAuthHandlerFactory {
-    private JsonObject config;
+    private final JsonObject config;
 
     public GithubAuthHandlerFactory(JsonObject config) {
         this.config = config;
@@ -17,12 +19,15 @@ public class GithubAuthHandlerFactory implements IAuthHandlerFactory {
 
     @Override
     public AuthHandler createAuthHandler(Vertx vertx, Route callbackRoute) {
+        if (isAuthDisabled(config)) {
+            return new NoopAuthHandler();
+        }
+
         final String clientId = config.getString("github_client_id");
         final String clientSecret = config.getString("github_client_secret");
         OAuth2Auth oauth2Provider = GithubAuth.create(vertx, clientId, clientSecret);
-        AuthHandler oauth2Handler = OAuth2AuthHandler.create(oauth2Provider)
+        return OAuth2AuthHandler.create(oauth2Provider)
                 .setupCallback(callbackRoute)
                 .addAuthority("user:email");
-        return oauth2Handler;
     }
 }
--- src/main/resources/localstack/init-aws.sh
+++ src/main/resources/localstack/init-aws.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-aws s3 --endpoint-url http://localhost:4566 mb s3://test-core-bucket
-aws s3 --endpoint-url http://localhost:4566 cp /s3/ s3://test-core-bucket/ --recursive
+aws s3 --endpoint-url http://localhost:5001 mb s3://test-core-bucket
+aws s3 --endpoint-url http://localhost:5001 cp /s3/ s3://test-core-bucket/ --recursive
