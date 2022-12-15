2f085dd05dc85d9bd245bdce944008c73b64fa53
Gian Miguel Del Mundo <gianmiguel.delmundo@thetradedesk.com>
2022-12-15 15:00:58 +0800

    Added logging in ResponseUtil

--- src/main/java/com/uid2/admin/vertx/ResponseUtil.java
+++ src/main/java/com/uid2/admin/vertx/ResponseUtil.java
@@ -3,11 +3,17 @@ package com.uid2.admin.vertx;
 import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 
 public class ResponseUtil {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ResponseUtil.class);
+
     public static void error(RoutingContext rc, int statusCode, String message) {
+        LOGGER.error(message);
+
         final JsonObject json = new JsonObject(new HashMap<String, Object>() {
             {
                 put("status", "error");
