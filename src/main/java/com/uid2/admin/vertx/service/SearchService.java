322d5f568860a1131c38767e1fe1b3a586e5d090
Thomas Manson <thomas.manson@thetradedesk.com>
2023-06-19 17:49:03 +1000

    Added search for client key and tests

--- src/main/java/com/uid2/admin/vertx/service/SearchService.java
+++ src/main/java/com/uid2/admin/vertx/service/SearchService.java
@@ -1,33 +1,65 @@
 package com.uid2.admin.vertx.service;
 
+import com.uid2.admin.vertx.ResponseUtil;
+import com.uid2.shared.auth.ClientKey;
 import com.uid2.shared.auth.Role;
 import com.uid2.shared.middleware.AuthMiddleware;
+import com.uid2.shared.store.reader.RotatingClientKeyProvider;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.vertx.core.http.HttpHeaders;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
 
-public class SearchService implements IService{
+import java.util.Optional;
+
+public class SearchService implements IService {
+    private static final String queryParameter = "keyOrSecret";
+
     private static final Logger LOGGER = LoggerFactory.getLogger(SearchService.class);
     private final AuthMiddleware auth;
+    private final RotatingClientKeyProvider clientKeyProvider;
 
-    public SearchService(AuthMiddleware auth) {
+    public SearchService(
+            AuthMiddleware auth,
+            RotatingClientKeyProvider clientKeyProvider) {
         this.auth = auth;
+        this.clientKeyProvider = clientKeyProvider;
     }
+
     @Override
     public void setupRoutes(Router router) {
-        router.get("/api/search/keyOrSecret").handler(
-                auth.handle(this::handleSearchKeyOrSecret, Role.ADMINISTRATOR));
+        router.get("/api/search").handler(
+                auth.handle(this::handleSearch, Role.ADMINISTRATOR));
     }
 
-    private void handleSearchKeyOrSecret(RoutingContext rc) {
+    private void handleSearch(RoutingContext rc) {
         try {
             LOGGER.debug("Starting Search Key Or Secret");
+            if (!rc.queryParams().contains(queryParameter)) {
+                ResponseUtil.error(rc, 400, "Invalid parameters");
+                return;
+            }
+            final String queryParam = rc.queryParam("keyOrSecret").get(0);
+            if (queryParam.length() < 6) {
+                ResponseUtil.error(rc, 400, "Parameter too short");
+            }
+
+            JsonArray results = new JsonArray();
+
+            Optional<ClientKey> client = this.clientKeyProvider.getAll()
+                    .stream().filter(c -> c.getKey().contains(queryParam))
+                    .findFirst();
+            if (client.isPresent()) {
+                results.add(client.get());
+            }
+
             rc.response()
                     .putHeader(HttpHeaders.CONTENT_TYPE, "application/json")
-                    .end("OK");
+                    .end(results.encode());
         } catch (Exception ex) {
             // does this go to some handler that removes sensitive info?
             rc.fail(500, ex);
--- src/test/java/com/uid2/admin/vertx/SearchServiceTest.java
+++ src/test/java/com/uid2/admin/vertx/SearchServiceTest.java
@@ -3,38 +3,46 @@ package com.uid2.admin.vertx;
 import com.uid2.admin.vertx.service.IService;
 import com.uid2.admin.vertx.service.SearchService;
 import com.uid2.admin.vertx.test.ServiceTestBase;
+import com.uid2.shared.auth.ClientKey;
 import com.uid2.shared.auth.Role;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.client.HttpResponse;
 import io.vertx.junit5.VertxTestContext;
+import org.instancio.Instancio;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static org.instancio.Select.field;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SearchServiceTest extends ServiceTestBase {
-    private final static String searchUrl = "api/search/keyOrSecret";
+    private final static String searchUrl = "api/search?keyOrSecret=";
+
     @Override
     protected IService createService() {
-        return new SearchService(auth);
+        return new SearchService(auth, clientKeyProvider);
     }
 
     @ParameterizedTest
     @MethodSource("nonAdminRolesTestData")
-    void searchAsNonAdminFails(Role role, Vertx vertx, VertxTestContext testContext){
+    void searchAsNonAdminFails(Role role, Vertx vertx, VertxTestContext testContext) {
         fakeAuth(role);
         get(vertx, searchUrl, expectHttpError(testContext, 401));
     }
 
     @Test
-    void searchAsAdminPasses(Vertx vertx, VertxTestContext testContext){
+    void searchAsAdminPasses(Vertx vertx, VertxTestContext testContext) {
         fakeAuth(Role.ADMINISTRATOR);
-        get(vertx, searchUrl, response -> {
+        get(vertx, searchUrl + "123456", response -> {
             assertTrue(response.succeeded());
             HttpResponse httpResponse = response.result();
             assertEquals(200, httpResponse.statusCode());
@@ -43,11 +51,130 @@ public class SearchServiceTest extends ServiceTestBase {
     }
 
     @Test
-    void searchWithoutRoleFails(Vertx vertx, VertxTestContext testContext){
+    void searchWithoutRoleFails(Vertx vertx, VertxTestContext testContext) {
         get(vertx, searchUrl, expectHttpError(testContext, 401));
     }
 
-    static Stream<Arguments> nonAdminRolesTestData(){
+    @Test
+    void searchWithoutQueryStringReturns400Error(Vertx vertx, VertxTestContext testContext) {
+        fakeAuth(Role.ADMINISTRATOR);
+        get(vertx, "api/search?invalid=123", response -> {
+            assertTrue(response.succeeded());
+            HttpResponse httpResponse = response.result();
+            assertEquals(400, httpResponse.statusCode());
+            String body = httpResponse.bodyAsString();
+            assertEquals("{\"message\":\"Invalid parameters\",\"status\":\"error\"}", body);
+            testContext.completeNow();
+        });
+    }
+
+    @ParameterizedTest
+    @MethodSource("shortParameterValues")
+    void searchWithShortQueryStringReturns400Error(String parameter, Vertx vertx, VertxTestContext testContext) {
+        fakeAuth(Role.ADMINISTRATOR);
+        get(vertx, String.format("api/search?keyOrSecret=$s", parameter), response -> {
+            assertTrue(response.succeeded());
+            HttpResponse httpResponse = response.result();
+            assertEquals(400, httpResponse.statusCode());
+            String body = httpResponse.bodyAsString();
+            assertEquals("{\"message\":\"Parameter too short\",\"status\":\"error\"}", body);
+            testContext.completeNow();
+        });
+    }
+
+    @Test
+    void searchClientKeyFindsKey(Vertx vertx, VertxTestContext testContext) throws InterruptedException {
+        fakeAuth(Role.ADMINISTRATOR);
+        ClientKey[] clientKeys = new ClientKey[3];
+        clientKeys[0] = new ClientKey("LOCALfCXrMMfsR3mDqAXELtWWMS+xG1s7RdgRTMqdOH2qaAo=", "DzBzbjTJcYL0swDtFs2krRNu+g1Eokm2tBU4dEuD0Wk=")
+                .withNameAndContact("MegaTest Client")
+                .withRoles(Role.OPERATOR)
+                .withSiteId(5);
+        clientKeys[1] = new ClientKey("LOCALt32pCM5NCX1E94UgOd2f8zhsKmxzCoyhXohHYSSWR8U=", "FsD4bvtjMkeTonx6HvQp6u0EiI1ApGH4pIZzZ5P7UcQ=")
+                .withNameAndContact("MegaTest Client 2")
+                .withRoles(Role.MAPPER)
+                .withSiteId(5);
+        clientKeys[2] = new ClientKey("LOCALH8VwqXl2G4TCuUWYAqdqkeG/UqtFoPEoXirKn4kHWxc=", "NcMgi6Y8C80SlxvV7pYlfcvEIo+2b0508tYQ3pKK8HM=")
+                .withNameAndContact("TestCorp Client")
+                .withRoles(Role.OPERATOR)
+                .withSiteId(5);
+
+        setClientKeys(clientKeys);
+        get(vertx, searchUrl + "fCXrMMfs", response -> {
+            assertTrue(response.succeeded());
+            HttpResponse httpResponse = response.result();
+            JsonArray result = httpResponse.bodyAsJsonArray();
+            assertEquals(1, result.size());
+            JsonObject client = result.getJsonObject(0);
+            AssertClientKey(clientKeys[0], client);
+            testContext.completeNow();
+        });
+    }
+
+    @ParameterizedTest
+    @MethodSource("clientsToFindBySearchString")
+    void searchClientKeyFindsKeyNew(ClientKey[] clientKeys, String searchString, Vertx vertx, VertxTestContext testContext) {
+        fakeAuth(Role.ADMINISTRATOR);
+
+        setClientKeys(clientKeys);
+
+        get(vertx, searchUrl + searchString, response -> {
+            try {
+                assertTrue(response.succeeded());
+                HttpResponse httpResponse = response.result();
+                JsonArray result = httpResponse.bodyAsJsonArray();
+                assertEquals(1, result.size());
+                JsonObject client = result.getJsonObject(0);
+                AssertClientKey(clientKeys[1], client);
+                testContext.completeNow();
+            } catch (Throwable ex) {
+                testContext.failNow(ex);
+            }
+        });
+    }
+
+    private static Stream<Arguments> clientsToFindBySearchString() {
+        ClientKey[] clientKeys = getClientKeys();
+        String key = clientKeys[1].getKey();
+        return Stream.of(
+                Arguments.of(clientKeys, key.substring(0, 8)),
+                Arguments.of(clientKeys, key.substring(key.length() - 8, key.length())),
+                Arguments.of(clientKeys, key.substring(10, 20)),
+                Arguments.of(clientKeys, key)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("clientsToNotFindByCase")
+    void searchClientKeyDoesNotFindDifferentCase(ClientKey[] clientKeys, String searchString, Vertx vertx, VertxTestContext testContext) {
+        fakeAuth(Role.ADMINISTRATOR);
+
+        setClientKeys(clientKeys);
+
+        get(vertx, searchUrl + searchString, response -> {
+            try {
+                assertTrue(response.succeeded());
+                HttpResponse httpResponse = response.result();
+                JsonArray result = httpResponse.bodyAsJsonArray();
+                assertEquals(0, result.size());
+                testContext.completeNow();
+            } catch (Throwable ex) {
+                testContext.failNow(ex);
+            }
+        });
+    }
+
+    private static Stream<Arguments> clientsToNotFindByCase() {
+        ClientKey[] clientKeys = getClientKeys();
+        String key = clientKeys[1].getKey();
+        return Stream.of(
+                Arguments.of(clientKeys, key.toLowerCase()),
+                Arguments.of(clientKeys, key.toUpperCase())
+        );
+    }
+
+
+    private static Stream<Arguments> nonAdminRolesTestData() {
         return Stream.of(
                 Arguments.of(Role.GENERATOR),
                 Arguments.of(Role.MAPPER),
@@ -61,5 +188,41 @@ public class SearchServiceTest extends ServiceTestBase {
                 Arguments.of(Role.SHARING_PORTAL),
                 Arguments.of(Role.PRIVATE_SITE_REFRESHER)
         );
+    }
+
+    private static Stream<Arguments> shortParameterValues() {
+        return Stream.of(
+                Arguments.of("a"),
+                Arguments.of("aa"),
+                Arguments.of("aaa"),
+                Arguments.of("aaaa"),
+                Arguments.of("aaaaa"),
+                Arguments.of("aaaaaa")
+        );
+    }
+
+    private static void AssertClientKey(ClientKey expected, JsonObject actual) {
+        assertEquals(expected.getKey(), actual.getString("key"));
+        assertEquals(expected.getSecret(), actual.getString("secret"));
+        assertEquals(expected.getName(), actual.getString("name"));
+        assertEquals(expected.getContact(), actual.getString("contact"));
+        assertEquals(expected.getSiteId(), actual.getInteger("site_id"));
+
+        List<Role> actualRoles = actual.getJsonArray("roles").stream()
+                .map(r -> Role.valueOf((String) r))
+                .collect(Collectors.toList());
+        assertEquals(expected.getRoles().size(), actualRoles.size());
+        for (Role role : expected.getRoles()) {
+            assertTrue(actualRoles.contains(role));
+        }
+    }
+
+    private static ClientKey[] getClientKeys() {
+        ClientKey[] clientKeys = Instancio.ofList(ClientKey.class)
+                .size(3)
+                .generate(field(ClientKey::getKey), gen -> gen.string().minLength(44).mixedCase())
+                .generate(field(ClientKey::getSecret), gen -> gen.string().minLength(44).mixedCase())
+                .create().toArray(new ClientKey[0]);
+        return clientKeys;
     }
 }
