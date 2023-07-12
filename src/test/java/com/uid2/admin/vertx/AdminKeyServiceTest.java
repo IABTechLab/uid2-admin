8b4aaeb4266e3ff4f54a8736441d7c9a4ebd0f7c
Thomas Manson <thomas.manson@thetradedesk.com>
2023-07-12 16:48:18 +1000

    Added unit tests to ensure admin uses the new format

---
+++ src/test/java/com/uid2/admin/vertx/AdminKeyServiceTest.java
@@ -0,0 +1,63 @@
+package com.uid2.admin.vertx;
+
+import com.uid2.admin.auth.AdminUser;
+import com.uid2.admin.vertx.service.AdminKeyService;
+import com.uid2.admin.vertx.service.IService;
+import com.uid2.admin.vertx.test.ServiceTestBase;
+import com.uid2.shared.auth.Role;
+import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.client.HttpResponse;
+import io.vertx.junit5.VertxTestContext;
+import org.instancio.Instancio;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.List;
+
+import static org.instancio.Select.field;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+
+public class AdminKeyServiceTest extends ServiceTestBase {
+    final String adminKeyPrefix = "UID2-A-L-";
+    @Override
+    protected IService createService() {
+        this.config.put("admin_key_prefix", adminKeyPrefix);
+        return new AdminKeyService(config, auth, writeLock, adminUserStoreWriter, adminUserProvider, keyGenerator, clientKeyStoreWriter, encryptionKeyStoreWriter, keyAclStoreWriter);
+    }
+
+    @Test
+    void addAdminUsesKeyPrefixAndFormattedKeyString(Vertx vertx, VertxTestContext testContext) throws Exception {
+        final String keySuffix = "abcdef.abcdefabcdefabcdef";
+        fakeAuth(Role.ADMINISTRATOR);
+        when(this.keyGenerator.generateFormattedKeyString(anyInt())).thenReturn(keySuffix);
+
+        AdminUser expectedAdminUser = this.getAdminUser(adminKeyPrefix + keySuffix);
+
+        post(vertx, String.format("api/admin/add?name=%s&roles=administrator", expectedAdminUser.getName()), "", response -> {
+           try {
+               HttpResponse<Buffer> httpResponse = response.result();
+               JsonObject result = httpResponse.bodyAsJsonObject();
+
+               assertAll(
+                       () -> assertTrue(response.succeeded()),
+                       () -> assertNotNull(result),
+                       () -> assertEquals(adminKeyPrefix + keySuffix, result.getString("key"))
+               );
+               testContext.completeNow();
+           } catch (Throwable t) {
+               testContext.failNow(t);
+           }
+        });
+    }
+
+    private AdminUser getAdminUser(String key) {
+        return Instancio.of(AdminUser.class)
+                .set(field(AdminUser::getKey), key)
+                .set(field(AdminUser::getRoles), new HashSet<Role>(List.of(Role.ADMINISTRATOR)))
+                .create();
+    }
+}
