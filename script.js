62d842131e8f8312650478b816474f7baeb628c6
Gian Miguel Del Mundo <gianmiguel.delmundo@thetradedesk.com>
2023-07-27 16:51:35 +0800

    Added benchmarking tests for key hashing

---
+++ script.js
@@ -0,0 +1,12 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+    vus: 20000,
+    duration: '30s',
+};
+
+export default function () {
+    http.get('http://localhost:8089/test');
+    sleep(1);
+}
--- src/main/java/com/uid2/admin/vertx/AdminVerticle.java
+++ src/main/java/com/uid2/admin/vertx/AdminVerticle.java
@@ -1,6 +1,8 @@
 package com.uid2.admin.vertx;
 
 import com.fasterxml.jackson.databind.ObjectWriter;
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import com.uid2.admin.auth.*;
 import com.uid2.admin.vertx.service.IService;
 import com.uid2.shared.Const;
@@ -20,6 +22,14 @@ import io.vertx.ext.web.codec.BodyCodec;
 import io.vertx.ext.web.handler.*;
 import io.vertx.ext.web.sstore.LocalSessionStore;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.time.Duration;
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+
 import static com.uid2.admin.auth.AuthUtil.isAuthDisabled;
 
 public class AdminVerticle extends AbstractVerticle {
@@ -31,6 +41,16 @@ public class AdminVerticle extends AbstractVerticle {
     private final IService[] services;
     private final ObjectWriter jsonWriter = JsonUtil.createJsonWriter();
 
+    private final List<KeyGenerationResult> validKgrs = new ArrayList<>();
+    private final List<KeyGenerationResult> allKgrs = new ArrayList<>();
+    private final Set<String> snapshotKeyHashes = new HashSet<>();
+    private final Set<String> snapshotKeys = new HashSet<>();
+
+    private final Cache<String, Boolean> comparisonResultCache = Caffeine.newBuilder()
+            .expireAfterWrite(Duration.ofMinutes(1))
+            .recordStats()
+            .build();
+
     public AdminVerticle(JsonObject config,
                          AuthFactory authFactory,
                          IAdminUserProvider adminUserProvider,
@@ -39,6 +59,18 @@ public class AdminVerticle extends AbstractVerticle {
         this.authFactory = authFactory;
         this.adminUserProvider = adminUserProvider;
         this.services = services;
+
+        int clientCount = 500;
+        for (int i = 0; i < clientCount; i++) {
+            validKgrs.add(this.generateFormattedKeyStringAndKeyHash(32));
+        }
+        snapshotKeyHashes.addAll(validKgrs.stream().map(KeyGenerationResult::getKeyHash).collect(Collectors.toSet()));
+        snapshotKeys.addAll(validKgrs.stream().map(KeyGenerationResult::getKey).collect(Collectors.toSet()));
+
+        allKgrs.addAll(validKgrs);
+        for (int i = 0; i < (clientCount*10/7)*3; i++) {
+            allKgrs.add(this.generateFormattedKeyStringAndKeyHash(32));
+        }
     }
 
     public void start(Promise<Void> startPromise) {
@@ -73,12 +105,106 @@ public class AdminVerticle extends AbstractVerticle {
         router.get("/ops/healthcheck").handler(this::handleHealthCheck);
         router.get("/api/token/get").handler(ctx -> handleTokenGet(ctx, oauth2Provider));
         router.get("/protected").handler(this::handleProtected);
+        router.get("/test").handler(this::test);
 
         for (IService service : this.services) {
             service.setupRoutes(router);
         }
 
         return router;
+    }
+
+    private void test(RoutingContext rc) {
+        int i = ThreadLocalRandom.current().nextInt(0, allKgrs.size());
+        String inputKey = "UID2-C-L-" + i + "-" + allKgrs.get(i).getKey();
+        snapshotKeys.contains(inputKey);
+//        Boolean cachedResult = comparisonResultCache.getIfPresent(inputKey);
+//        if (cachedResult == null) {
+//            boolean result = false;
+//            for (String snapshotKeyHash : snapshotKeyHashes) {
+//                result = this.compareFormattedKeyStringAndKeyHash(inputKey, snapshotKeyHash);
+//                if (result) {
+//                    break;
+//                }
+//            }
+//            comparisonResultCache.put(inputKey, result);
+//        }
+
+        rc.response().end("OK");
+    }
+
+    private void test2(RoutingContext rc) {
+
+        rc.response().end("OK");
+    }
+
+    private byte[] generateRandomKey(int keyLen) {
+        final SecureRandom random = new SecureRandom();
+        final byte[] bytes = new byte[keyLen];
+        random.nextBytes(bytes);
+        return bytes;
+    }
+
+    private String generateRandomKeyString(int keyLen) {
+        return Utils.toBase64String(generateRandomKey(keyLen));
+    }
+
+    private KeyGenerationResult generateFormattedKeyStringAndKeyHash(int keyLen) {
+        String key = this.generateRandomKeyString(keyLen);
+        String formattedKey = key.length() >= 6 ? new StringBuilder(key).insert(6, ".").toString() : key;
+
+        byte[] keyBytes = Utils.decodeBase64String(key);
+        byte[] saltBytes = generateRandomKey(keyLen);
+
+        MessageDigest md = createMessageDigest();
+        md.update(saltBytes);
+        byte[] hashBytes = md.digest(keyBytes); // This will always generate a byte array of length 512 (64 bytes)
+
+        String hash = Utils.toBase64String(hashBytes); // This will always generate a String with 88 chars (86 + 2 padding)
+        String salt = Utils.toBase64String(saltBytes);
+        String keyHash = String.format("%s$%s", hash, salt);
+
+        return new KeyGenerationResult(formattedKey, keyHash);
+    }
+
+    private boolean compareFormattedKeyStringAndKeyHash(String formattedKey, String keyHash) {
+        String inputKey = formattedKey.substring(formattedKey.lastIndexOf("-") + 1).replace(".", "");
+        byte[] inputKeyBytes = Utils.decodeBase64String(inputKey);
+
+        byte[] hashBytes = Utils.decodeBase64String(keyHash.split("\\$")[0]);
+        byte[] saltBytes = Utils.decodeBase64String(keyHash.split("\\$")[1]);
+
+        MessageDigest md = createMessageDigest();
+        md.update(saltBytes);
+        byte[] inputHashBytes = md.digest(inputKeyBytes);
+
+        return Arrays.equals(inputHashBytes, hashBytes);
+    }
+
+    private MessageDigest createMessageDigest() {
+        try {
+            return MessageDigest.getInstance("SHA-512");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static class KeyGenerationResult {
+        private final String key;
+        private final String keyHash;
+
+        public KeyGenerationResult(String key, String keyHash) {
+            this.key = key;
+            this.keyHash = keyHash;
+        }
+
+        public String getKey() {
+            return key;
+        }
+
+        public String getKeyHash() {
+            return keyHash;
+        }
     }
 
     private void handleHealthCheck(RoutingContext rc) {
