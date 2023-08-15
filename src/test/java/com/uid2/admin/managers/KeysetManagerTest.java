21074cca91cbf33622466ae9e7a2581c95b53dfb
Cody Constine <cody.constine@thetradedesk.com>
2023-08-15 12:07:58 -0600

    Adding the last test

--- src/test/java/com/uid2/admin/managers/KeysetManagerTest.java
+++ src/test/java/com/uid2/admin/managers/KeysetManagerTest.java
@@ -3,8 +3,10 @@ package com.uid2.admin.managers;
 import com.uid2.admin.secret.IKeysetKeyManager;
 import com.uid2.admin.store.writer.KeysetStoreWriter;
 import com.uid2.admin.util.KeysetUtil;
+import com.uid2.shared.auth.ClientKey;
 import com.uid2.shared.auth.Keyset;
 import com.uid2.shared.auth.KeysetSnapshot;
+import com.uid2.shared.auth.Role;
 import com.uid2.shared.store.reader.RotatingKeysetProvider;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,6 +15,7 @@ import org.mockito.ArgumentMatcher;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.Set;
 import java.util.Map;
@@ -80,10 +83,36 @@ public class KeysetManagerTest {
     }
 
     @Test
-    public void testCreateKeysetForClient() {
+    public void testCreateKeysetForClient() throws Exception {
+        KeysetManager keysetManager = new KeysetManager(keysetProvider, keysetStoreWriter,
+                keysetKeyManager, true);
+
+        Map<Integer, Keyset> keysets = new HashMap<Integer, Keyset>() {{
+            put(1, KeysetUtil.createDefaultKeyset(3, 1));
+            put(2, KeysetUtil.createDefaultKeyset(4, 2));
+            put(3, KeysetUtil.createDefaultKeyset(5, 3));
+            put(4, KeysetUtil.createDefaultKeyset(6, 4));
+        }};
+
+        setKeysets(keysets);
         // Sharer makes an empty list
+        ClientKey sharer = new ClientKey("", "", "",  "", Instant.now(), Set.of(Role.SHARER), 7, false);
+        keysetManager.createKeysetForClient(sharer);
+        Keyset sharerKeyset = keysets.get(5);
+        assertEquals(sharerKeyset.getAllowedSites(), Set.of());
         // Generator makes a null list
+        ClientKey generator = new ClientKey("", "", "",  "", Instant.now(), Set.of(Role.GENERATOR), 8, false);
+        keysetManager.createKeysetForClient(generator);
+        Keyset generatorKeyset = keysets.get(6);
+        assertNull(generatorKeyset.getAllowedSites());
         // Generator takes priority of sharer
+        ClientKey sharerGenerator = new ClientKey("", "", "",  "", Instant.now(), Set.of(Role.SHARER, Role.GENERATOR), 9, false);
+        keysetManager.createKeysetForClient(sharerGenerator);
+        Keyset bothKeyset = keysets.get(7);
+        assertNull(bothKeyset.getAllowedSites());
+        // If keyset already exists none gets added
+        keysetManager.createKeysetForClient(sharer);
+        assertFalse(keysets.containsKey(8));
     }
 
     protected static class MapOfSize implements ArgumentMatcher<Map> {
