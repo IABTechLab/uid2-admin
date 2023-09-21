3f79d992b2b130a8ea470066cac420936c8cbe21
Gian Miguel Del Mundo <gianmiguel.delmundo@thetradedesk.com>
2023-09-21 16:34:33 +0800

    Converted JsonUtil ObjectWriter to singleton

--- src/main/java/com/uid2/admin/vertx/JsonUtil.java
+++ src/main/java/com/uid2/admin/vertx/JsonUtil.java
@@ -6,14 +6,25 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.uid2.shared.util.Mapper;
 
 public class JsonUtil {
-    public static ObjectWriter createJsonWriter() {
-        ObjectMapper mapper = new ObjectMapper()
-                .registerModule(new JavaTimeModule())
-                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+    private static final ObjectWriter INSTANCE;
+
+    static {
         DefaultPrettyPrinter pp = new DefaultPrettyPrinter();
         pp.indentArraysWith(DefaultIndenter.SYSTEM_LINEFEED_INSTANCE);
-        return mapper.writer(pp);
+
+        INSTANCE = Mapper.getInstance()
+                .registerModule(new JavaTimeModule())
+                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+                .writer(pp);
+    }
+
+    private JsonUtil() {
+    }
+
+    public static ObjectWriter createJsonWriter() {
+        return INSTANCE;
     }
 }
