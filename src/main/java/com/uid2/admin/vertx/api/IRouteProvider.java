8d9cb2d24cef75087c2da59270f509add2ba13b0
Lionell Pack <lionell.pack@thetradedesk.com>
2023-09-27 13:26:24 +1000

    Provide some useful comments.

--- src/main/java/com/uid2/admin/vertx/api/IRouteProvider.java
+++ src/main/java/com/uid2/admin/vertx/api/IRouteProvider.java
@@ -5,9 +5,13 @@ import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 
 /*
- Implement this interface to automatically be picked up by V2Router and have your routes registered under /v2api/*.
+ Implement this interface to automatically be picked up by V2Router and have your routes registered under /v2api/.
  Any constructor dependencies which are registered should be auto-injected by Guice, as long as it knows about them.
  You must have a constructor marked with @Inject for DI to use it.
+
+ *Important*
+ If you implement this interface, your route will be registered as a non-blocking handler. Use IBlockingRouteProvider
+ instead if you want to provide a blocking handler. See `readme.md` for more information.
  */
 public interface IRouteProvider {
     Handler<RoutingContext> getHandler();
