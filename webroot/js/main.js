25f983127fc4c9e65e64445adf796db0e5875b87
Thomas Manson <thomas.manson@thetradedesk.com>
2024-06-03 09:31:45 +1000

    Added some missing javascript deleted in the merge

--- webroot/js/main.js
+++ webroot/js/main.js
@@ -6,7 +6,7 @@ function doApiCall(method, url, outputDiv, errorDiv, body) {
         type: method,
         url: url,
         dataType: "text",
-        data : body,
+        data: body,
         success: text => setOutput(text, outputDiv),
         error: err => standardErrorCallback(err, errorDiv)
     });
@@ -17,7 +17,7 @@ function doApiCallWithCallback(method, url, onSuccess, onError, body) {
         type: method,
         url: url,
         dataType: "text",
-        data : body,
+        data: body,
         success: text => onSuccess(text),
         error: err => onError(err)
     });
@@ -28,27 +28,32 @@ function setOutput(text, div) {
     $(div).text(pretty);
 }
 
-function errorCallback(err) { 
-    standardErrorCallback(err, "#errorOutput") 
+function errorCallback(err) {
+    standardErrorCallback(err, "#errorOutput")
 }
 
 function standardErrorCallback(err, errorDiv, onError) {
-    if(err.getResponseHeader("REQUIRES_AUTH") == 1) {
+    if (err.getResponseHeader("REQUIRES_AUTH") == 1) {
         $("body").hide()
         $("body").replaceWith("Unauthorized, prompting reauthentication...")
         $("body").show()
         $(function () {
-            setTimeout(function() {
+            setTimeout(function () {
                 window.location.replace("/login");
             }, 3000);
         });
     } else {
-        if(onError === undefined) {
+        if (onError === undefined) {
             $(errorDiv).text("Error: " + err.status + ": " + (isJsonString(err.responseText) ? JSON.parse(err.responseText).message : (err.responseText ? err.responseText : err.statusText)));
         } else {
             onError(err);
         }
     }
+}
+
+function alertErrorCallback(err, errorDiv, alert) {
+    $(errorDiv).text("Error: " + err.status + ": " + (isJsonString(err.responseText) ? JSON.parse(err.responseText).message : (err.responseText ? err.responseText : err.statusText)));
+    $(alert).collapse('show');
 }
 
 function isJsonString(str) {
@@ -58,6 +63,19 @@ function isJsonString(str) {
         return false;
     }
     return true;
+}
+
+function clearFormsAndShowCollapsed(toShow) {
+    const exclude = toShow.join(',');
+    $(".form-clear").not(exclude).text("");
+    $("form").trigger("reset");
+    $('.collapse').not(exclude).collapse('hide');
+
+    setTimeout(function () {
+        toShow.forEach((regionId) => {
+            $(regionId).collapse('show');
+        })
+    }, 400);  // the collapse takes 350ms, so wait until all are done before showing them
 }
 
 function init() {
@@ -94,7 +112,7 @@ function init() {
             header.text(header.text().replace("Env", "Prod"));
         } else if (window.location.origin.includes("integ")) {
             header.text(header.text().replace("Env", "Integ"));
-        } else if (window.location.origin.includes("localhost")) {
+        } else if (window.location.origin.includes("localhost") || (window.location.origin.includes("127.0.0.1"))) {
             header.text(header.text().replace("Env", "Local"));
         } else {
             header.text(header.text().replace("Env", ""));
@@ -103,7 +121,18 @@ function init() {
         if (window.location.origin.endsWith(".eu")) {
             header.text(header.text().replace("UID2", "EUID"));
         }
+        
+        $(".group").on("click", (e) => {
+            $(".group").removeClass("btn-primary").addClass("btn-secondary");
+            
+            if ($("#" + e.target.id).attr("aria-expanded") == "false") {
+                $("#" + e.target.id).removeClass("btn-primary").addClass("btn-secondary");
+            } else {
+                $("#" + e.target.id).removeClass("btn-secondary").addClass("btn-primary");
+            }
+        })
     });
+
 }
 
 init();
