1678148997f54d57c98453e09acd54497878f0dd
Aleksandrs Ulme <aleksandrs.ulme@thetradedesk.com>
2025-03-27 09:41:05 +0800

    Creating cloud encryption keys based on operator keys, not sites

--- src/main/java/com/uid2/admin/cloudencryption/CloudKeyRotationStrategy.java
+++ src/main/java/com/uid2/admin/cloudencryption/CloudKeyRotationStrategy.java
@@ -2,8 +2,8 @@ package com.uid2.admin.cloudencryption;
 
 import com.google.common.collect.Streams;
 import com.uid2.admin.store.Clock;
+import com.uid2.shared.auth.OperatorKey;
 import com.uid2.shared.model.CloudEncryptionKey;
-import com.uid2.shared.model.Site;
 
 import java.util.Collection;
 import java.util.Map;
@@ -27,48 +27,29 @@ public class CloudKeyRotationStrategy {
         }
 
         public Set<CloudEncryptionKey> computeDesiredKeys(
-                Collection<CloudEncryptionKey> existingKeys,
-                Collection<Site> sites
+                Collection<CloudEncryptionKey> existingCloudKeys,
+                Collection<OperatorKey> operatorKeys
         ) {
-                var idGenerator = new KeyIdGenerator(existingKeys);
-                Map<Integer, Set<CloudEncryptionKey>> existingKeysBySite = existingKeys
+                var keyGenerator = new CloudEncryptionKeyGenerator(clock, secretGenerator, existingCloudKeys);
+                Map<Integer, Set<CloudEncryptionKey>> existingKeysBySite = existingCloudKeys
                         .stream()
                         .collect(Collectors.groupingBy(CloudEncryptionKey::getSiteId, Collectors.toSet()));
 
-                return sites
+                return operatorKeys
                         .stream()
-                        .map(Site::getId)
+                        .map(OperatorKey::getSiteId)
                         .distinct()
-                        .flatMap(siteId -> desiredKeysForSite(siteId, idGenerator, existingKeysBySite.getOrDefault(siteId, Set.of())))
+                        .flatMap(siteId -> desiredKeysForSite(siteId, keyGenerator, existingKeysBySite.getOrDefault(siteId, Set.of())))
                         .collect(Collectors.toSet());
         }
 
         private Stream<CloudEncryptionKey> desiredKeysForSite(
                 Integer siteId,
-                KeyIdGenerator idGenerator,
+                CloudEncryptionKeyGenerator keyGenerator,
                 Set<CloudEncryptionKey> existingKeys
         ) {
                 var existingKeysToRetain = keyRetentionStrategy.selectKeysToRetain(existingKeys);
-                var newKey = makeNewKey(siteId, idGenerator);
+                var newKey = keyGenerator.makeNewKey(siteId);
                 return Streams.concat(existingKeysToRetain.stream(), Stream.of(newKey));
-        }
-
-        private CloudEncryptionKey makeNewKey(Integer siteId, KeyIdGenerator idGenerator) {
-                var nowSeconds = clock.getEpochSecond();
-                var keyId = idGenerator.nextId();
-                var secret = secretGenerator.generate();
-                return new CloudEncryptionKey(keyId, siteId, nowSeconds, nowSeconds, secret);
-        }
-
-        private static class KeyIdGenerator {
-                private int lastId;
-
-                public KeyIdGenerator(Collection<CloudEncryptionKey> existingKeys) {
-                        this.lastId = existingKeys.stream().map(CloudEncryptionKey::getId).max(Integer::compareTo).orElse(0);
-                }
-
-                public int nextId() {
-                        return ++lastId;
-                }
         }
 }
