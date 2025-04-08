c7f943382e9ffc9a5b28dc410ad3ac67f08d83ce
abuabraham-ttd <130511456+abuabraham-ttd@users.noreply.github.com>
2025-04-07 21:09:11 -0700

    Abu fix metadata updates (#415)
    * Update to check keys and update
    
    ---------
    
    Co-authored-by: Release Workflow <unifiedid-admin+release@thetradedesk.com>

--- src/main/java/com/uid2/admin/store/writer/EncryptedSaltStoreWriter.java
+++ src/main/java/com/uid2/admin/store/writer/EncryptedSaltStoreWriter.java
@@ -2,6 +2,7 @@ package com.uid2.admin.store.writer;
 
 import com.uid2.admin.store.FileManager;
 import com.uid2.admin.store.version.VersionGenerator;
+import com.uid2.shared.Utils;
 import com.uid2.shared.cloud.TaggableCloudStorage;
 import com.uid2.shared.encryption.AesGcm;
 import com.uid2.shared.model.CloudEncryptionKey;
@@ -15,18 +16,19 @@ import org.slf4j.LoggerFactory;
 import io.vertx.core.json.JsonObject;
 
 import java.io.BufferedWriter;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Base64;
-import java.util.Collection;
-import java.util.List;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.*;
 
 public class EncryptedSaltStoreWriter extends SaltStoreWriter implements StoreWriter {
     private StoreScope scope;
     private RotatingCloudEncryptionKeyProvider cloudEncryptionKeyProvider;
     private Integer siteId;
+    private JsonObject unEncryptedMetadataData;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(EncryptedSaltStoreWriter.class);
     public EncryptedSaltStoreWriter(JsonObject config, RotatingSaltProvider provider, FileManager fileManager,
@@ -43,12 +45,46 @@ public class EncryptedSaltStoreWriter extends SaltStoreWriter implements StoreWr
         return scope.resolve(new CloudPath("salts.txt." + snapshot.getEffective().toEpochMilli())).toString();
     }
 
+    private CloudEncryptionKey getActiveCloudEncryptionKey(){
+        try {
+            return cloudEncryptionKeyProvider.getEncryptionKeyForSite(siteId);
+        } catch (IllegalStateException e) {
+            LOGGER.error("Error: No Cloud Encryption keys available for encryption for site ID: {}", siteId, e);
+            throw e;
+        }
+    }
+
     @Override
-    protected void uploadSaltsSnapshot(RotatingSaltProvider.SaltSnapshot snapshot, String location) throws Exception {
+    protected JsonObject enrichMetadata(JsonObject metadata){
+        metadata.put("key_id", this.getActiveCloudEncryptionKey().getId());
+        return metadata;
+    }
+
+    /**
+     * Attempts to upload the salts snapshot to the specified location.
+     * <p>
+     * If the snapshot does not exist, it will be created at the given location.
+     * If it exists but was encrypted with a different key, the snapshot is
+     * re-encrypted and overwritten.
+     * </p>
+     * @param snapshot The snapshot containing the salts.
+     * @param location The target storage location.
+     * @return {@code true} if the snapshot was successfully written, {@code false} otherwise.
+     */
+    @Override
+    protected boolean tryUploadSaltsSnapshot(RotatingSaltProvider.SaltSnapshot snapshot, String location) throws Exception {
         if (siteId == null) {
             throw new IllegalStateException("Site ID is not set.");
         }
-
+        CloudEncryptionKey encryptionKey = this.getActiveCloudEncryptionKey();
+        boolean fileExist = !cloudStorage.list(location).isEmpty();
+         if (fileExist) {
+             JsonObject metadata = Utils.toJsonObject(this.cloudStorage.download(this.getMetadataPath().toString()));
+             if (Objects.equals(metadata.getInteger("key_id", null), encryptionKey.getId())) {
+                 LOGGER.info("Not overwriting salt files for site {} as encryption key is already used before", this.siteId);
+                 return false;
+             }
+         }
         StringBuilder stringBuilder = new StringBuilder();
 
         for (SaltEntry entry: snapshot.getAllRotatingSalts()) {
@@ -56,13 +92,6 @@ public class EncryptedSaltStoreWriter extends SaltStoreWriter implements StoreWr
         }
 
         String data = stringBuilder.toString();
-
-        CloudEncryptionKey encryptionKey = null;
-        try {
-            encryptionKey = cloudEncryptionKeyProvider.getEncryptionKeyForSite(siteId);
-        } catch (IllegalStateException e) {
-            LOGGER.error("Error: No Cloud Encryption keys available for encryption for site ID: {}", siteId, e);
-        }
         JsonObject encryptedJson = new JsonObject();
         if (encryptionKey != null) {
             byte[] secret = Base64.getDecoder().decode(encryptionKey.getSecret());
@@ -78,15 +107,21 @@ public class EncryptedSaltStoreWriter extends SaltStoreWriter implements StoreWr
         try (BufferedWriter w = Files.newBufferedWriter(newSaltsFile)) {
             w.write(encryptedJson.encodePrettily());
         }
-
         this.upload(newSaltsFile.toString(), location);
+        return true;
+    }
+
+    @Override
+    protected JsonObject getMetadata(){
+        return this.unEncryptedMetadataData;
     }
 
     @Override
     public void upload(Object data, JsonObject extraMeta) throws Exception {
+        this.unEncryptedMetadataData = extraMeta;
         @SuppressWarnings("unchecked")
         List<RotatingSaltProvider.SaltSnapshot> snapshots = new ArrayList<>((Collection<RotatingSaltProvider.SaltSnapshot>) data);
-        this.buildAndUploadMetadata(extraMeta, this.uploadSnapshotsAndGetMetadata(snapshots));
+        this.buildAndUploadMetadata(snapshots);
     }
 
     @Override
