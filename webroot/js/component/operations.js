883c08444428902f43e029c027be7392fd23cb8e
Aleksandrs Ulme <aleksandrs.ulme@thetradedesk.com>
2025-06-25 12:31:43 +0800

    remove unused function

--- webroot/js/component/operations.js
+++ webroot/js/component/operations.js
@@ -307,30 +307,6 @@ function syncSharedInputValues(changedInput) {
     });
 }
 
-// function syntaxHighlightJson(json) {
-//     if (typeof json !== 'string') {
-//         json = JSON.stringify(json, null, 2);
-//     }
-//
-//     json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-//
-//     return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-//         let cls = 'json-number';
-//         if (/^"/.test(match)) {
-//             if (/:$/.test(match)) {
-//                 cls = 'json-key';
-//             } else {
-//                 cls = 'json-string';
-//             }
-//         } else if (/true|false/.test(match)) {
-//             cls = 'json-boolean';
-//         } else if (/null/.test(match)) {
-//             cls = 'json-null';
-//         }
-//         return '<span class="' + cls + '">' + match + '</span>';
-//     });
-// }
-
 function initializeExecuteHandlers(config) {
     if (!document.body.hasAttribute('data-execute-handlers-initialized')) {
         document.body.setAttribute('data-execute-handlers-initialized', 'true');
